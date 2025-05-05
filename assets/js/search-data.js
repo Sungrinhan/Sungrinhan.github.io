539ca718a55901d157d8ed93db0e52f47c0d7d4b
Sungrinhan <78065205+Sungrinhan@users.noreply.github.com>
2025-05-05 12:49:31 +0000

    Deploying to gh-pages from @ Sungrinhan/Sungrinhan.github.io@d8b5462dac9ae9f60579644af474ae49b40141ee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -80,17 +80,6 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2025/2025-05-04-%EC%A3%BC%EC%A0%9C-react%EC%99%80-redux%EC%97%90%EC%84%9C-immer%EB%A5%BC-%ED%99%9C%EC%9A%A9%ED%95%9C-%EB%B6%88%EB%B3%80%EC%84%B1-%EA%B4%80%EB%A6%AC-%EB%B8%94%EB%A1%9C%EA%B7%B8-%EC%A0%9C%EB%AA%A9-immer%EB%A1%9C-react%EC%99%80-redux%EC%97%90%EC%84%9C-%EB%B6%88%EB%B3%80%EC%84%B1%EC%9D%84-%EC%89%BD%EA%B2%8C-%EA%B4%80%EB%A6%AC%ED%95%98%EB%A9%B0-%EC%BD%94%EB%93%9C-%ED%92%88%EC%A7%88-%EB%86%92%EC%9D%B4%EA%B8%B0/";
-          
-        },
-      },{id: "post-",
-        
-          title: "",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
             window.location.href = "/blog/2025/2025-05-01-%EC%A3%BC%EC%A0%9C-redux%EC%99%80-typescript%EC%9D%98-%EC%97%B0%EA%B3%84-%EC%82%AC%EC%9A%A9-%EB%B8%94%EB%A1%9C%EA%B7%B8-%EC%A0%9C%EB%AA%A9-redux%EC%99%80-typescript%EB%A5%BC-%ED%99%9C%EC%9A%A9%ED%95%9C-%EB%8D%94-%EC%95%88%EC%A0%95%EC%A0%81%EC%9D%B8-%EC%83%81%ED%83%9C-%EA%B4%80%EB%A6%AC-%EB%B0%A9%EB%B2%95%EC%97%90-%EB%8C%80%ED%95%9C-%EB%94%A5%EB%8B%A4%EC%9D%B4%EB%B8%8C/";
           
         },
@@ -105,9 +94,9 @@ ninja.data = [{
             window.location.href = "/blog/2025/2025-04-28-%EC%A3%BC%EC%A0%9C-next.js%EC%97%90%EC%84%9C%EC%9D%98-server-side-rendering-%EB%B8%94%EB%A1%9C%EA%B7%B8-%EC%A0%9C%EB%AA%A9-next.js%EC%97%90%EC%84%9C%EC%9D%98-server-side-rendering-%ED%8D%BC%ED%8F%AC%EB%A8%BC%EC%8A%A4-%ED%96%A5%EC%83%81%EA%B3%BC-seo-%EC%B5%9C%EC%A0%81%ED%99%94%EB%A1%9C-%EB%A6%AC%EB%8D%94%ED%95%9C-%EA%B1%B8%EC%9D%8C-%EB%8D%94-%EB%82%98%EC%95%84%EA%B0%80%EA%B8%B0/";
           
         },
-      },{id: "post-주제-추천-react-native를-활용한-크로스-플랫폼-모바일-앱-개발-블로그-제목-react-native를-이용해-본격-크로스-플랫폼-모바일-앱-개발에-도전하기",
+      },{id: "post-react-native를-이용해-본격-크로스-플랫폼-모바일-앱-개발에-도전하기",
         
-          title: "주제 추천: React Native를 활용한 크로스 플랫폼 모바일 앱 개발 블로그 제목: React...",
+          title: "React Native를 이용해 본격 크로스 플랫폼 모바일 앱 개발에 도전하기",
         
         description: "",
         section: "Posts",
@@ -116,9 +105,20 @@ ninja.data = [{
             window.location.href = "/blog/2025/%EC%A3%BC%EC%A0%9C-%EC%B6%94%EC%B2%9C-react-native%EB%A5%BC-%ED%99%9C%EC%9A%A9%ED%95%9C-%ED%81%AC%EB%A1%9C%EC%8A%A4-%ED%94%8C%EB%9E%AB%ED%8F%BC-%EB%AA%A8%EB%B0%94%EC%9D%BC-%EC%95%B1-%EA%B0%9C%EB%B0%9C-%EB%B8%94%EB%A1%9C%EA%B7%B8-%EC%A0%9C%EB%AA%A9-react-native%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%B4-%EB%B3%B8%EA%B2%A9-%ED%81%AC%EB%A1%9C%EC%8A%A4-%ED%94%8C%EB%9E%AB%ED%8F%BC-%EB%AA%A8%EB%B0%94%EC%9D%BC-%EC%95%B1-%EA%B0%9C%EB%B0%9C%EC%97%90-%EB%8F%84%EC%A0%84%ED%95%98%EA%B8%B0/";
           
         },
-      },{id: "post-graphql과-apollo를-활용한-효율적인-데이터-관리-블로그-제목-react에서-graphql과-apollo를-활용한-첨단-데이터-관리-방법",
+      },{id: "post-immer로-react와-redux에서-불변성을-쉽게-관리하며-코드-품질-높이기",
         
-          title: "GraphQL과 Apollo를 활용한 효율적인 데이터 관리 블로그 제목: React에서 GraphQL과 Apollo를 활용한 첨단...",
+          title: "Immer로 React와 Redux에서 불변성을 쉽게 관리하며 코드 품질 높이기",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/%EC%A3%BC%EC%A0%9C-react%EC%99%80-redux%EC%97%90%EC%84%9C-immer%EB%A5%BC-%ED%99%9C%EC%9A%A9%ED%95%9C-%EB%B6%88%EB%B3%80%EC%84%B1-%EA%B4%80%EB%A6%AC-%EB%B8%94%EB%A1%9C%EA%B7%B8-%EC%A0%9C%EB%AA%A9-immer%EB%A1%9C-react%EC%99%80-redux%EC%97%90%EC%84%9C-%EB%B6%88%EB%B3%80%EC%84%B1%EC%9D%84-%EC%89%BD%EA%B2%8C-%EA%B4%80%EB%A6%AC%ED%95%98%EB%A9%B0-%EC%BD%94%EB%93%9C-%ED%92%88%EC%A7%88-%EB%86%92%EC%9D%B4%EA%B8%B0/";
+          
+        },
+      },{id: "post-react에서-graphql과-apollo를-활용한-첨단-데이터-관리-방법",
+        
+          title: "React에서 GraphQL과 Apollo를 활용한 첨단 데이터 관리 방법",
         
         description: "",
         section: "Posts",
